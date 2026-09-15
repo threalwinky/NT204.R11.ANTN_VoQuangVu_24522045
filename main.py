0f721b14a2826abb5e0082f6232f8f6be170803b
threalwinky <voquangvu09112006@gmail.com>
2026-09-15 23:28:02 +0700

    Add interface and pcap options

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-test
+import argparse
+from scapy.all import sniff, rdpcap
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--interface")
+    group.add_argument("--pcap")
+    args = parser.parse_args()
+
+    if args.interface:
+        print(f"Sniffing on {args.interface}...")
+        sniff(iface=args.interface, prn=lambda pkt: print(pkt.summary()), store=0)
+    elif args.pcap:
+        print(f"Reading from {args.pcap}...")
+        for pkt in rdpcap(args.pcap):
+            print(pkt.summary())
+
+if __name__ == "__main__":
+    main()
