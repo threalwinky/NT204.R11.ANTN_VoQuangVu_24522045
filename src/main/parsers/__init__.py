3ccbb77f31afb68f2d89664765fbad654de9bb7d
threalwinky <voquangvu09112006@gmail.com>
2026-09-20 15:20:38 +0700

    implement IPv4 packet parser

---
+++ src/main/parsers/__init__.py
@@ -0,0 +1,3 @@
+from .ipv4 import parse_ipv4
+
+__all__ = ["parse_ipv4"]
---
+++ src/main/parsers/ipv4.py
@@ -0,0 +1,20 @@
+from scapy.layers.inet import IP
+from scapy.packet import Packet
+
+from main.models import NormalizedIDSEvent
+
+def parse_ipv4(
+    packet: Packet,
+    event: NormalizedIDSEvent,
+) -> NormalizedIDSEvent:
+    
+    if not packet.haslayer(IP):
+        return event
+
+    ip = packet[IP]
+
+    event.network_protocol = "IPv4"
+    event.src_ip = ip.src
+    event.dst_ip = ip.dst
+
+    return event
